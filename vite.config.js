fdf929bb460582ae04f248ed1ada8ab9ee4988b6
山崎智央 <tomohiro@MacbookAirM3.local>
2026-02-19 19:06:16 +0900

    fix: フルパスbase設定 + alert追加

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './',
+    base: 'https://tomohiro-hubs.github.io/inet_r/',
     plugins: [react()],
     publicDir: 'public',
     build: {
