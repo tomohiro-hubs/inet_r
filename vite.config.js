578c20cc3e06e9fa365357be4bed0bdec72fe7a5
山崎智央 <tomohiro@MacbookAirM3.local>
2026-02-19 19:12:20 +0900

    fix: GH Pages設定完了に伴い、デバッグコード削除して本番Reactアプリに復元 (base: './' + HashRouter)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'https://tomohiro-hubs.github.io/inet_r/',
+    base: './',
     plugins: [react()],
     publicDir: 'public',
     build: {
