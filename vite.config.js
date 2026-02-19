ea02031f30d3f1a359fd93acd19d508d2252dcce
山崎智央 <tomohiro@MacbookAirM3.local>
2026-02-19 18:50:35 +0900

    fix: HashRouterに変更 + base: './' に変更してデプロイ環境を選ばず動くように + .nojekyll追加

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/inet_r/',
+    base: './',
     plugins: [react()],
     publicDir: 'public',
     build: {
