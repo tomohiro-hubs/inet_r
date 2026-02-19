14576fad1a3480b2e169ccd43c94c2ff08e4e456
山崎智央 <tomohiro@MacbookAirM3.local>
2026-02-19 19:00:58 +0900

    chore: debug mode - index.htmlにエラーハンドラ追加 & main.jsxをHello Worldに簡略化

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,14 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
+        <div style={{ padding: 20, textAlign: 'center' }}>
+            <h1>React Works! (Debug Mode)</h1>
+            <p>If you see this, React is mounting correctly.</p>
+        </div>
+        {/*
         <HashRouter>
             <App />
         </HashRouter>
+        */}
     </React.StrictMode>
 );
