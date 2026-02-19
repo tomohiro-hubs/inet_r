cdbfef7ccfda0b2fba641a293d78b3fab9f9a675
山崎智央 <tomohiro@MacbookAirM3.local>
2026-02-19 18:53:43 +0900

    fix: GH Pagesでの404回避のため、ビルド生成ファイル名を固定（ハッシュ除外）

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
     publicDir: 'public',
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`,
+            },
+        },
     },
     server: {
         port: 3000,
